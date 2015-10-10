d8846229c64197750a0624d6bd4cdc8e11353805
sakapun <sakapun5th@gmail.com>
2015-10-11 01:28:22 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var cli = require('cheerio-httpcli');
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/search/', function(req, res){
+	var pref = '15';
+	var mount = '';
+	if(req.query.mount){
+		mount = req.query.mount;
+		cli.fetch('http://tenkura.n-kishou.co.jp/tk/kanko/kad.html', { code: mount, type: '15', ba: 'hr' }, function (err, $, result, body) {
+			if(err){
+				res.status('403');
+				res.send('the mountain is not defined' + err);
+			} else {
+				var gotHtml = $(".kad_tbl2").eq(2).find("tr.t_w.mnt_td").html();
+				if(gotHtml){
+					res.status('200');
+					res.send(JSON.stringify(gotHtml) );
+				} else {
+					res.status('403');
+					console.log(req.query.mount);
+				}
+			}
+		})
+	} else {
+		res.status('403');
+		res.send('mount is required');
+	}
+});
+app.listen(3000);
